91f48379972d2a2f85f58457654cb607050cc59a
frostime <frostime@foxmail.com>
2024-05-08 15:53:57 +0800

    🔨 把之前 global-this 中而合并到 data-query

--- src/func/data-query.ts
+++ src/func/data-query.ts
@@ -3,7 +3,7 @@
  * @Author       : frostime
  * @Date         : 2024-05-08 15:00:37
  * @FilePath     : /src/func/data-query.ts
- * @LastEditTime : 2024-05-08 15:27:14
+ * @LastEditTime : 2024-05-08 15:41:53
  * @Description  :
  *      - Fork from project https://github.com/zxhd863943427/siyuan-plugin-data-query
  *      - 基于该项目的 v0.0.7 版本进行修改
@@ -201,6 +201,76 @@ export class DataView {
     }
 }
 
+/**************************************** Query 函数 ****************************************/
+
+import { sql } from "@/api";
+
+/**
+ * Filter blocks in sql search scenario to eliminate duplicate blocks
+ * @param blocks Block[]
+ * @param mode uni 模式;
+ *  - 'leaf' 只返回叶子节点
+ *  - 'root' 只返回根节点
+ * @param para_in_li boolean, 如果为 True 则对在 li 中的 p 节点进行处理
+ * @param ret 返回类型, 'block' 返回 Block[], 'id' 返回 BlockId[]
+ * @returns BlockId[]
+ */
+function UniBlocks(blocks: Block[], mode: 'leaf' | 'root' = 'leaf', para_in_li: boolean = true, ret: "block" | "id" ="block") {
+    console.log('UniBlocks', blocks);
+    let p2c = new Map();
+    let blocksMap = new Map();
+    blocks.forEach(block => {
+        p2c.set(block.parent_id, block.id);
+        blocksMap.set(block.id, block);
+    });
+    let blockIdsResult: BlockId[] = [];
+    const pushResult = (block: Block) => {
+        if (!blockIdsResult.includes(block.id)) {
+            blockIdsResult.push(block.id);
+        }
+    };
+    if (mode === 'root') {
+        for (let block of blocks) {
+            // 找到 block 最顶层的 parent 节点
+            while (blocksMap.has(block.parent_id)) {
+                block = blocksMap.get(block.parent_id);
+            }
+            pushResult(block);
+        }
+    }
+    else if (mode === 'leaf') {
+        for (let block of blocks) {
+            //如果 block 不在 parent 集合中，则 block 为叶子节点
+            if (!p2c.has(block.id)) {
+                //如果是 li 内的 p 节点, 则
+                if (para_in_li && block.type === 'p') {
+                    let parent = blocksMap.get(block.parent_id);
+                    if (parent && parent.type === 'i') {
+                        block = parent;
+                    }
+                }
+                pushResult(block);
+            }
+        }
+    }
+    let retBlocks = blockIdsResult.map(id => blocksMap.get(id));
+    return ret === "block" ? retBlocks : retBlocks.map(block => block.id);
+}
+
+async function GetBlocksByIDs(...ids: BlockId[]) {
+    let idList = ids.map((id) => `"${id}"`);
+    let sqlCode = `select * from blocks where id in (${idList.join(",")})`;
+    let data = await sql(sqlCode);
+    if (!data) {
+        return data;
+    }
+    let dataMap = {};
+    for (let block of data) {
+        dataMap[block.id] = block;
+    }
+    return dataMap;
+}
+
 /**************************************** Func ****************************************/
 
 
@@ -209,10 +279,20 @@ export let enabled = false;
 
 export const load = () => {
     if (enabled) return;
-    lute = setLute({});
 
+    // lute = setLute({});
     globalThis.newDV = (protyle: IProtyle, item: HTMLElement, top: number | null) => {
+        if (!lute) {
+            lute = setLute({});
+        }
         return new DataView(protyle, item, top);
+    }
+    globalThis.Query = {
+        UniBlocks,
+        GetBlocksByIDs,
+        sql: sql,
+        b2link: (b: Block) => `[${b.content}](siyuan://blocks/${b.id})`,
+        b2ref: (b: Block) => `((${b.id} '${b.content}'))`
     }
 
     enabled = true;
@@ -220,7 +300,10 @@ export const load = () => {
 
 export const unload = () => {
     if (!enabled) return;
+
     lute = null;
     delete globalThis.newDV;
+    delete globalThis.Query;
+
     enabled = false;
 }
