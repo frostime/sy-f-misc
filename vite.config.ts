2a508825adaed5ad44dbc8d7938f35cbeb87a8b4
frostime <frostime@foxmail.com>
2024-05-21 20:16:55 +0800

    🔧 添加 svelte 依赖

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from "vite"
 import minimist from "minimist"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 import livereload from "rollup-plugin-livereload"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
 import zipPack from "vite-plugin-zip-pack";
 import fg from 'fast-glob';
 
@@ -22,6 +23,8 @@ export default defineConfig({
     },
 
     plugins: [
+
+        svelte(),
 
         viteStaticCopy({
             targets: [
