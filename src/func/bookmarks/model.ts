b19c51d401b00ed7edc7ed7b8f8c2791295882ac
frostime <frostime@foxmail.com>
2024-05-28 12:11:15 +0800

    🚧 实现获取块和文档信息的 API，以及实现 promise pool

--- src/func/bookmarks/model.ts
+++ src/func/bookmarks/model.ts
@@ -1,6 +1,68 @@
+// import { writable } from "svelte/store";
+
 import type FMiscPlugin from "@/index";
+import { sql, request } from "@/api";
+import PromiseLimitPool from "@/utils/promise-pool";
 
 const StorageNameBookmarks = 'bookmarks';
+
+/**
+ * 传入若干个 block id, 返回一个对象, key 为 block id, value 为 block
+ * @param ids 
+ * @returns 
+ */
+const getBlocks = async (...ids: BlockId[]) => {
+    const fmt = `select * from blocks where id in (${ids.map((b) => `'${b}'`).join(',')})`;
+    let blocks = await sql(fmt);
+    let results: { [key: BlockId]: Block | null } = {};
+    for (let id of ids) {
+        let block = blocks.find(block => block.id === id);
+        if (block) {
+            results[id] = block;
+        } else {
+            results[id] = null;
+        }
+    }
+    return results;
+}
+
+interface IDocInfo {
+    rootID: DocumentId;
+    rootIcon: string;
+    rootTitle: string;
+    box: string;
+}
+/**
+ * 传入若干个 doc id, 返回一个对象, key 为 doc id, value 为 doc info
+ * @param docIds 
+ * @returns 
+ */
+const getDocInfos = async (...docIds: DocumentId[]) => {
+    const endpoints = '/api/block/getBlockInfo';
+    const pool = new PromiseLimitPool<IDocInfo>(16);
+    for (let docId of docIds) {
+        pool.add(async () => {
+            const result = await request(endpoints, { id: docId });
+            return result;
+        });
+    }
+    let results: { [key: DocumentId]: IDocInfo | null } = {};
+    try {
+        let docInfos = await pool.awaitAll();
+        for (let id of docIds) {
+            let docInfo = docInfos.find(docInfo => docInfo.rootID === id);
+            if (docInfo) {
+                results[id] = docInfo;
+            } else {
+                results[id] = null;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    return results;
+}
+
 
 export class BookmarkDataModel {
     plugin: FMiscPlugin;
@@ -24,7 +86,7 @@ export class BookmarkDataModel {
         await this.plugin.saveData(StorageNameBookmarks + '.json', this.plugin.data.bookmarks);
     }
 
-    listGroups(visible: boolean = true): IBookmarkGroup[]{
+    listGroups(visible: boolean = true): IBookmarkGroup[] {
         // 1. sort
         let groups = Array.from(this.bookmarks.values());
         groups.sort((a, b) => a.order - b.order);
