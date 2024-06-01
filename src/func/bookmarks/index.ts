1ab7a48c6fb6d936d1323db5c708c18626ca09d1
frostime <frostime@foxmail.com>
2024-06-01 14:22:23 +0800

    🐛 fix(bookmark): 有时候无法正常添加 dock

--- src/func/bookmarks/index.ts
+++ src/func/bookmarks/index.ts
@@ -3,7 +3,7 @@
  * @Author       : frostime
  * @Date         : 2024-05-19 21:52:48
  * @FilePath     : /src/func/bookmarks/index.ts
- * @LastEditTime : 2024-05-28 16:13:08
+ * @LastEditTime : 2024-06-01 14:19:08
  * @Description  : 
  */
 import type FMiscPlugin from "@/index";
@@ -16,6 +16,8 @@ let bookmark: Bookmark;
 let model: BookmarkDataModel;
 
 const initBookmark = async (ele: HTMLElement, plugin: FMiscPlugin) => {
+    await model.load();
+    await model.updateItems();
     bookmark = new Bookmark({
         target: ele,
         props: {
@@ -44,8 +46,9 @@ export const load = async (plugin: FMiscPlugin) => {
 
     model = getModel(plugin);
 
-    await model.load();
-    await model.updateItems();
+    //疑似首先 await load 会导致 addDock 无法正常执行
+    // await model.load();
+    // await model.updateItems();
 
     insertStyle('hide-bookmark', `
     .dock span[data-type="bookmark"] {
