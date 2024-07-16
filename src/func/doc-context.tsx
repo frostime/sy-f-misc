6215c5af0a051def07878b940c7ad950d532527d
frostime <frostime@foxmail.com>
2024-07-16 21:25:45 +0800

    ✨ feat: 优化 doc context，增加快捷键在文档上下文中移动

--- src/func/doc-context.tsx
+++ src/func/doc-context.tsx
@@ -3,16 +3,17 @@
  * @Author       : frostime
  * @Date         : 2024-06-10 14:55:35
  * @FilePath     : /src/func/doc-context.tsx
- * @LastEditTime : 2024-06-26 21:34:08
+ * @LastEditTime : 2024-07-16 21:24:28
  * @Description  : 
  */
 import { For, Show } from 'solid-js';
 import { render } from 'solid-js/web';
-import { type Plugin, type Dialog, openTab } from "siyuan";
+import { type Dialog, openTab, showMessage, IProtyle } from "siyuan";
 
 import { simpleDialog } from "@/libs/dialog";
 import { getBlockByID, listDocsByPath } from "@/api";
 import { getActiveDoc, getNotebook } from "@/utils";
+import type FMiscPlugin from '..';
 
 
 let I18n: any = {
@@ -40,6 +41,21 @@ const listChildDocs = async (doc: any) => {
     let data = await listDocsByPath(doc.box, doc.path);
     // console.log(data);
     return data?.files;
+}
+
+const getSibling = async (path: string, box: string) => {
+    path = path.replace('.sy', '');
+    const parts = path.split('/');
+
+    if (parts.length > 0) {
+        parts.pop();
+    }
+
+    let parentPath = parts.join('/');
+    parentPath = parentPath || '/';
+
+    let siblings = await listChildDocs({path: parentPath, box});
+    return siblings;
 }
 
 const createContext = async () => {
@@ -163,18 +179,20 @@ const DocContextComponent = (props: {
 };
 
 
-let plugin_: Plugin;
+let plugin_: FMiscPlugin;
 // const keymapTag = window.siyuan.config.keymap.general.tag;
 const Keymap = '⌥S';
 
+const KeymapConfig = window.siyuan.config.keymap;
+
 export let name = "DocContext";
 export let enabled = false;
-export const load = (plugin: Plugin) => {
+export const load = (plugin: FMiscPlugin) => {
     if (enabled) return;
     enabled = true;
     plugin_ = plugin;
     plugin.addCommand({
-        langKey: 'F-Misc::DocContext',
+        langKey: 'fmisc::DocContext',
         langText: `F-misc ${I18n.name}`,
         hotkey: Keymap,
         callback: async () => {
@@ -197,11 +215,126 @@ export const load = (plugin: Plugin) => {
             container.style.setProperty('max-height', '75%');
         }
     });
+
+    let lastTriggered: Date = new Date();
+    /**
+     * 控制时间，如果 Action 间隔太短，就关掉中键的文档
+     * @returns 
+     */
+    const speedControl = () => {
+        let now = new Date();
+        let closeCurrentDoc = () => {};
+        if ((now.getTime() - lastTriggered.getTime()) <= 1000) {
+            let tab = document.querySelector("div.layout__wnd--active ul.layout-tab-bar>li.item--focus");
+            let closeEle = tab.querySelector('span.item__close') as HTMLSpanElement;
+            closeCurrentDoc = () => closeEle.click();
+        }
+        lastTriggered = now;
+        return closeCurrentDoc;
+    }
+
+    const goToSibling = async (delta: -1 | 1) => {
+        let docId = getActiveDoc();
+        if (!docId) return 
+        let doc = await getBlockByID(docId);
+        let { path, box } = doc;
+
+        let siblings: {id: string, path: string}[] = await getSibling(path, box);
+        let index = siblings.findIndex(sibling => sibling.path === path);
+        if ((delta < 0 && index == 0) || (delta > 0 && index == siblings.length - 1)) {
+            showMessage(`跳转${delta < 0 ? '最后' : '第'}一篇文档`);
+        }
+
+        let postAction = speedControl();
+
+        let newIndex = (index + delta + siblings.length) % siblings.length;
+        openTab({
+            app: plugin.app,
+            doc: {
+                id: siblings[newIndex].id
+            }
+        });
+        postAction();
+    }
+
+    const goToParent = async () => {
+        let docId = getActiveDoc();
+        if (!docId) return 
+        let doc = await getBlockByID(docId);
+        let parent = await getParentDocument(doc.path);
+        if (!parent) {
+            showMessage('无父文档');
+            return;
+        }
+
+        let postAction = speedControl();
+        openTab({
+            app: plugin.app,
+            doc: {
+                id: parent.id
+            }
+        });
+        postAction();
+    }
+
+    const goToChild = async () => {
+        let docId = getActiveDoc();
+        if (!docId) return;
+
+        let doc = await getBlockByID(docId);
+        let children = await listChildDocs(doc);
+        if (children.length === 0) {
+            showMessage('无子文裆');
+            return;
+        }
+
+        let postAction = speedControl();
+        openTab({
+            app: plugin.app,
+            doc: {
+                id: children[0].id
+            }
+        });
+        postAction();
+    }
+
+    plugin.addCommand({
+        langKey: 'fmisc::last-doc',
+        langText: '上一篇文档',
+        hotkey: '⌘←',
+        callback: async () => goToSibling(-1)
+    });
+    plugin.addCommand({
+        langKey: 'fmisc::next-doc',
+        langText: '下一篇文档',
+        hotkey: '⌘→',
+        callback: async () => goToSibling(1)
+    });
+    plugin.addCommand({
+        langKey: 'fmisc::parent-doc',
+        langText: '父文档',
+        hotkey: '⌘↑',
+        callback: async () => goToParent()
+    });
+    plugin.addCommand({
+        langKey: 'fmisc::child-doc',
+        langText: '子文档',
+        hotkey: '⌘↓',
+        callback: async () => goToChild()
+    });
+    KeymapConfig.editor.general.collapse.custom = '';
+    KeymapConfig.editor.general.expand.custom = '';
 }
 
-export const unload = (plugin: Plugin) => {
+export const unload = (plugin: FMiscPlugin) => {
     if (!enabled) return;
     enabled = false;
     plugin_ = null;
-    plugin.commands = plugin.commands.filter((command) => command.langKey !== 'F-Misc::DocContext');
+
+    plugin.delCommand('fmisc::DocContext')
+    plugin.delCommand('fmisc::last-doc')
+    plugin.delCommand('fmisc::next-doc')
+
+    KeymapConfig.editor.general.collapse.custom = KeymapConfig.editor.general.collapse.default;
+    KeymapConfig.editor.general.expand.custom = KeymapConfig.editor.general.expand.default;
 }
