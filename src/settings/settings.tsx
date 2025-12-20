7f0aa21b1f756b37f42c857a578a531f6b03f5f4
frostime <frostime@foxmail.com>
2025-12-21 00:03:50 +0800

    🐛 fix bugs

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -84,7 +84,7 @@ const App: Component<IArgs> = (props) => {
 
     const CustomModuleConfigs = () => (
         <>
-            <For each={props.customModuleConfigs.filter(config => config.items && config.items.length > 0) || []}>
+            <For each={props.customModuleConfigs.filter(config => config.items || config.customPanel)}>
                 {(config) => (
                     <div style={{
                         margin: '5px 24px',
